7b9e58d1c58a6953cde6d7c7c1aae579b36a41d0
anariosp91 <ana-rios91@hotmail.com>
2022-04-16 09:42:33 -0500

    creando el servidor y las rutas

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(8000, (req,resp)=> console.log('iniciando servidor en el puerto 8000'));
+
+const publicPath = path.resolve(__dirname,'./public')
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+	res.sendFile(path.join(__dirname, '/views/index.html'));
+})
+app.get('/login', (req,res) => {
+	res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+app.get('/productCart', (req,res) => {
+	res.sendFile(path.join(__dirname, '/views/productCart.html'));
+})
+app.get('/productDetail', (req,res) => {
+	res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+})
+app.get('/register', (req,res) => {
+	res.sendFile(path.join(__dirname, '/views/register.html'));
+})
