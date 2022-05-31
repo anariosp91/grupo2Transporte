73a768353cf82a5520b557a732086b733dce2f40
anariosp91 <ana-rios91@hotmail.com>
2022-05-30 20:22:13 -0500

    empezando a crear las validaciones del formulario de creacion de tours

---
+++ website/middlewares/validationTours.js
@@ -0,0 +1,35 @@
+const {check} = require('express-validator');
+const toursController = require('../controllers/toursController');
+const path = require('path');
+
+const validationTours = [
+    check('nombre').notEmpty().withMessage('Debes completar este campo'),
+    check('descripcionCorta').isLength({ min: 80, max: 100 }).withMessage('La descripción debe contener entre 80 y 100 caracteres'),
+    check('descripcionDetallada').isLength({ min: 150, max: 200 }).withMessage('La descripción debe contener entre 150 y 200 caracteres'),
+    check('duracion')
+    .notEmpty().withMessage('Debes completar este campo')
+    .isInt().withMessage('Este campo debe ser un número entero'),
+    check('precio')
+    .notEmpty().withMessage('Debes completar este campo')
+    .isFloat({ min: 100000 })
+] 
+   
+   
+    
+    
+
+   
+
+
+// function isImage(imagen) {
+//     let imagen = check('imagen1');
+//     let extension = (path.extname(imagen)).toLowerCase();
+//     let extensionesPermitidas = ['.jpg', 'png', 'gif', 'webp'];
+//     if(extensionesPermitidas.includes(extension)){
+
+//     }
+    
+    
+// }
+
+module.exports = validationTours
--- website/routers/toursRouter.js
+++ website/routers/toursRouter.js
@@ -1,30 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+// const multer = require('multer');
 const path = require('path');
-
-const toursController = require('../controllers/toursController')
-
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-            cb(null, 'public/img/products')
-    },
-    
-    filename:  (req, file, cb) => {
-            let newFileName = 'tour-' + Date.now() + path.extname(file.originalname) ;
-            cb(null, newFileName);
-    }  
-})    
-
-
-const upload = multer({ storage: storage}   )
+const {check, body, validationResult} = require('express-validator');
+const toursController = require('../controllers/toursController');
+const upload = require('../middlewares/multer');
+const validationTours = require('../middlewares/validationTours');
 
 
 router.get('/detail/:id/', toursController.detail);
 
 //creacion del producto
 router.get('/create', toursController.create);
-router.post('/create', upload.any(), toursController.save)
+router.post('/create', upload.any(), validationTours, toursController.save)
 
 router.get('/edit/:id/', toursController.edit);
 router.put('/edit/:id/', upload.any(), toursController.update)
