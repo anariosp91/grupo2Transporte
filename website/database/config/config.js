71060cd4631846e03770bf2ff03c48a4555ee59b
anariosp91 <ana-rios91@hotmail.com>
2022-07-02 17:51:55 -0500

    actualizando el retro y el weekly

---
+++ website/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": 'secret',
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": 'secret',
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": 'secret',
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
