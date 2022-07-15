c9d13533db24b2be3d8110b8d2a5aa46d8b32dd2
anariosp91 <ana-rios91@hotmail.com>
2022-07-15 15:16:30 -0500

    corrigiendo la carga de imagenes en el metodo de creacion de tours

--- website/controllers/toursControllerSequelize.js
+++ website/controllers/toursControllerSequelize.js
@@ -16,9 +16,12 @@ let toursController = {
                oldData: req.body
         })
         }
-
+        console.log(req.body, req.files)
         db.Tour.create({
             ...req.body,
+            image1 : req.files[0].filename,
+            image2 : req.files[1].filename,
+            image3 : req.files[2].filename
         })
         .then(tour => res.redirect('/tours/list'))
     },
