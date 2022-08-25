d1a2a06b00d3067586d460ac188aeece2fcdfed5
anariosp91 <ana-rios91@hotmail.com>
2022-08-25 17:34:36 -0500

    corrigiendo errores en el middleware usserlogged

--- website/middlewares/userlogged.js
+++ website/middlewares/userlogged.js
@@ -1,34 +1,33 @@
-//const User = require('../models/User');
+
 const db = require ('../database/models/index');
 
 function userLogged(req, res, next) {
 	res.locals.isLogged = false;
 	if(req.cookies.userEmail){
 		let emailInCookie = req.cookies.userEmail;
-		
-
+	
 		db.User.findOne({
 			where: {email: emailInCookie}
 		})
 		.then(userFromCookie => {
-
-		if (userFromCookie) {
-			req.session.userLogged = userFromCookie;
-		}
-	
-		if (req.session.userLogged) {
-			res.locals.isLogged = true;
-			res.locals.userLogged = req.session.userLogged;
-		}
-	})
+			if (userFromCookie) {
+				req.session.userLogged = userFromCookie;
+			}
+		
+			if (req.session.userLogged) {
+				res.locals.isLogged = true;
+				res.locals.userLogged = req.session.userLogged;
+			}
+			next()
+		})
 	}else if(req.session.userLogged){
 		res.locals.isLogged = true;
 		res.locals.userLogged = req.session.userLogged;
-		res.clearCookie('userEmail')
-	}
-	
 
-	next();
+		next()
+	}else{
+		next()
+	}
 }
 
 module.exports = userLogged;
